80a354c7503701f8cf387cf5916c1d9866e7f419
founticle <121849029+founticle@users.noreply.github.com>
2023-01-03 12:38:26 +0530

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,3 +1,10 @@
+/* 
+-Time Complexity : Worse case: O(n2)
+                 : Best case: O(n)
+    
+-Space Complexity : O(1), since constant amount of additional memory is used apart from inpurt array.
+*/                    
+
 class BubbleSort {
     public static void print_Array(int array[]){
         for(int i=0; i<array.length; i++){
@@ -9,7 +16,7 @@ class BubbleSort {
         int array[] = {9,7,5,3,2,12,4};
 
         
-    //     // bubble sort
+      // bubble sort
         for(int i=0; i<array.length-1; i++){
             for(int j=0; j<array.length-i-1; j++){
                 if(array[j] > array[j+1]){
